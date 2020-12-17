2038a6bfed503bf63d50e69471895e2bba54eadc
Karthikeyan <>
2020-12-17 10:39:17 +0530

    Initial commit

---
+++ src/test/java/com/karthik/cicd/TechDockerJenkinsSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karthik.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechDockerJenkinsSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
